d5ce2ea8ce161de407de2d70a025b5a37a4f290f
Guille <ghervas88@gmail.com>
2022-12-26 13:07:59 +0100

    hello world

---
+++ src/main/java/api/demo/FilmSpringAppApplication.java
@@ -0,0 +1,13 @@
+package api.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmSpringAppApplication.class, args);
+	}
+
+}
